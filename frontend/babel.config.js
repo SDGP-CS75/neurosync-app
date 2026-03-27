f0356c52d707b5c5134e3ce2ccc61b3b3232c2cc
ravindu212 <ravinduvidusara09@gmail.com>
2026-03-27 12:02:48 +0530

    fix expo go

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -3,9 +3,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      // Required for react-native-reanimated v4
-      "react-native-worklets/plugin"
-    ]
+    plugins: []
   };
 };
