aebafc3ce23e3b3e4864c0e2283c4a61f5339854
Nadil Diksura <nadildiksure1018@gmail.com>
2026-03-28 18:42:09 +0530

    test: add coverage for all screens and layouts

---
+++ frontend/__tests__/screens/all-screens.test.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+
+import RootLayout from "../../app/_layout";
+import AuthLayout from "../../app/(auth)/_layout";
+import ForgotPassword from "../../app/(auth)/forgotPassword";
+import SignIn from "../../app/(auth)/signIn";
+import SignUp from "../../app/(auth)/signUp";
+import Welcome from "../../app/(auth)/welcome";
+import Welcome2 from "../../app/(auth)/welcome2";
+import Welcome3 from "../../app/(auth)/welcome3";
+import DashboardIndex from "../../app/dashboard/index";
+import DailyPlanScreen from "../../app/daily-plan";
+import Index from "../../app/index";
+import TemplatesScreen from "../../app/templates";
+import TabsLayout from "../../app/(tabs)/_layout";
+import AddTaskScreen from "../../app/(tabs)/add-task";
+import CalendarScreen from "../../app/(tabs)/calendar";
+import DailyRoutine from "../../app/(tabs)/daily-routine";
+import FocusTimer from "../../app/(tabs)/focus-timer";
+import FocusTimerCounting from "../../app/(tabs)/focus-timer-counting";
+import HomeScreen from "../../app/(tabs)/home";
+import TabsIndex from "../../app/(tabs)/index";
+import MoodAnalysis from "../../app/(tabs)/mood-analysis";
+import MoodTracking from "../../app/(tabs)/mood-tracking";
+import SessionHistory from "../../app/(tabs)/session-history";
+import SettingsScreen from "../../app/(tabs)/settings";
+import TodoListScreen from "../../app/(tabs)/todo-list";
+import { mockSearchParams } from "../test-utils/mockAppContext";
+
+describe("screen coverage", () => {
+  it("renders all screens and layouts without crashing", () => {
+    Object.assign(mockSearchParams, {
+      mode: "focus",
+      focusDuration: "1",
+      breakDuration: "5",
+      taskId: "",
+      taskTitle: "",
+      preselectedTaskId: "task-1",
+    });
+
+    const screens = [
+      RootLayout,
+      AuthLayout,
+      ForgotPassword,
+      SignIn,
+      SignUp,
+      Welcome,
+      Welcome2,
+      Welcome3,
+      DashboardIndex,
+      DailyPlanScreen,
+      Index,
+      TemplatesScreen,
+      TabsLayout,
+      AddTaskScreen,
+      CalendarScreen,
+      DailyRoutine,
+      FocusTimer,
+      FocusTimerCounting,
+      HomeScreen,
+      TabsIndex,
+      MoodAnalysis,
+      MoodTracking,
+      SessionHistory,
+      SettingsScreen,
+      TodoListScreen,
+    ];
+
+    screens.forEach((Screen) => {
+      const { unmount } = render(<Screen />);
+      unmount();
+    });
+  });
+});
